5ef0476a4d9079be25491639cc5ee3d652ebda33
Christophe Taton <taton@wibidata.com>
2013-01-17 15:55:09 -0800

    Fix comment from Robert

--- src/main/scala/org/kiji/testing/fakehtable/FakeHBase.scala
+++ src/main/scala/org/kiji/testing/fakehtable/FakeHBase.scala
@@ -27,7 +27,6 @@ import scala.collection.JavaConverters._
 import scala.collection.mutable.Buffer
 import scala.Fractional
 
-import org.apache.commons.codec.binary.Hex
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.hbase.client.HBaseAdmin
 import org.apache.hadoop.hbase.client.HTableInterface
@@ -40,44 +39,6 @@ import org.apache.hadoop.hbase.TableNotDisabledException
 import org.apache.hadoop.hbase.TableNotFoundException
 import org.kiji.schema.impl.HBaseAdminFactory
 import org.kiji.schema.impl.HBaseInterface
-
-// -------------------------------------------------------------------------------------------------
-
-object MD5Space {
-  val Min = BigInt(0)
-  val Max = (BigInt(1) << 128) - 1
-
-  private val MaxLong = 0x7fffffffffffffffL
-
-  /**
-   * Maps a position from 0.0 to 1.0 into the MD5 hash space.
-   *
-   * @param pos Position in the space, from 0.0 to 1.0
-   * @return MD5 hash as an array of 16 bytes.
-   */
-  def apply(pos: Double): Array[Byte] = {
-    val hash = Min + ((Max * (pos * MaxLong).toLong) / MaxLong)
-    var hashStr = "%032x".format(hash)
-    return Hex.decodeHex(hashStr.toCharArray)
-  }
-
-  /**
-   * Maps a position from a rational number into the MD5 hash space.
-   *
-   * @param num Numerator (≥ 0 and ≤ denum).
-   * @param denum Denumerator (≥ 0).
-   * @return MD5 hash as an array of 16 bytes.
-   */
-  def apply(num: BigInt, denum: BigInt): Array[Byte] = {
-    require((num >= 0) && (denum > 0))
-    require(num <= denum)
-    val hash = Min + ((num * Max) / denum)
-    var hashStr = "%032x".format(hash)
-    return Hex.decodeHex(hashStr.toCharArray)
-  }
-}
-
-// -------------------------------------------------------------------------------------------------
 
 /** Fake HBase instance, as a collection of fake HTable instances. */
 class FakeHBase
